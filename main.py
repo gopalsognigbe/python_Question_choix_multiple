331c93e675781ec40735a38da9615b4eca7b8b2c
Gopal <gopalsognigbe@gmail.com>
2024-07-08 10:08:38 +0100

    Update main.py
    Amelioration du code avec des fonctions indépendantes et exclusion des erreurs

--- main.py
+++ main.py
@@ -1,41 +1,53 @@
 #Question a choix multiples 
 
-import random
-countries=['Benin','France','Togo','Italie']
-random.shuffle(countries)
+#definir les questions en respectant l'ordre (question,(proposition),(reponse correcte))
+question1=('Quelle est la capitale de France ?',('Paris','Rome','Porto-novo','Lomé','gopal'),'Paris')
+question2=('Quelle est la capitale de Italie ?',('Paris','Rome','Porto-novo','Lomé'),'Rome')
+question3=('Quelle est la capitale de Benin ?',('Paris','Rome','Porto-novo','Lomé'),'Porto-novo')
+question4=('Quelle est la capitale de Togo ?',('Paris','Rome','Porto-novo','Lomé'),'Lomé')
 
-score=0
-def question_capitale_reponse(pays):
-    print(f"Quel est la capitale de {pays}")
-    print("(a)-> Rome")
-    print("(b)-> Paris")
-    print("(c)-> Porto-Novo")
-    print("(d)-> Lomé")
-    reponse=input("Entrer votre choix entre (a,b,c.d) :")
-    return reponse
+questionnaire=[question1,question2,question3,question4]
 
 
-def verification_resultat( pays,reponse):
-    global score 
-    if pays == "Benin" and reponse == 'c':
-        print("Bonne reponse ")
-        score+=1
-    elif pays == 'Togo' and reponse == 'd':
-        print("Bonne reponse")
-        score+=1
-    elif pays == 'France' and reponse == 'b':
-        print("Bonne reponse")
-        score+=1
-    elif pays == 'Italie' and reponse == 'a':
-        print("Bonne reponse")
-        score+=1
-    else :
-        print("Mauvaise reponse")
+def poser_question(question):
+    print(f'{question[0]}')
+    for i in range (len(question[1])):
+        print(f'{i+1} -> {question[1][i]}')
+    index=verification_resultat(question)
+    if question[1][index] == question[2]:
+        print("Bonne reponse!!")
+        print("_________________________________")
+        return True
+    else:
+        print("Mauvaise reponse!")
+    print("_________________________________")
+    return False
 
-for i in countries :
-    rep= question_capitale_reponse(i)
-    verification_resultat(i,rep)
-    print("------------------------------")
-print("Votre score est de :" ,score, "sur ",4)
+#verification de la reponse de l'utilisateur et exclusion des erreurs potentiels 
+def verification_resultat(question):
+    reponse = input(f"Entrer la bonne reponse entre 1 et {len(question[1])} :")
+    try :
+        index=int(reponse)-1
+    except:
+        print("Erreur vous devez entrer un chiffre !!!")
+        print('')
+        return verification_resultat(question)
+    if not 0 <= index <=len(question[1]) :
+        print(f"Veuillez mettre un chiffre entre 1 et {len(question[1])} !!!")
+        print("")
+        return verification_resultat(question)
+    print("")
+    return index
+  
+#lancement des questions dans la liste quetionnairea et decompte du score 
+def lancer_question(questionnaire):
+    score=0
+    for question in questionnaire:
+        if poser_question(question) == True:
+            score +=1
+    print("Votre score est de :" ,score, "sur ",4)
+    
+lancer_question(questionnaire)
+
 
         
