7889bdd29a5a190985d1ca03437fdd4692a011c5
Gopal <gopalsognigbe@gmail.com>
2026-04-03 11:43:05 +0100

    new

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def lancer_question(questionnaire):
     for question in questionnaire :
         if poser_question(question) == True:
             score +=1
-    print("Votre score final est de regarde  :" 
+    print("Votre score final est de :" 
            ,score, "sur ",
            len(questionnaire    
                 )
